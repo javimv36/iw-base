764c0a0322cd8e1a2af0e41d4e39b0783b2690c2
Javier Martín <javmar12@ucm.es>
2018-03-05 12:29:29 +0100

    Empezando modelo de visita
    Decidir atributos necesarios

---
+++ src/main/java/es/ucm/fdi/iw/model/Visita.java
@@ -0,0 +1,19 @@
+package es.ucm.fdi.iw.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Visita {
+	private String direccion;
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+}
